d8a5a902d901c408ef3e022d6905a68b120a3d36
YTMartian <1773741250@qq.com>
2020-10-11 20:34:57 +0800

    ++

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -4,10 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.os.Build;
 import android.view.*;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.*;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -55,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         Calendar current = Calendar.getInstance();
         int currentYear = current.get(Calendar.YEAR);
         for (int year = currentYear - 1; year <= currentYear + 1; year++) {
-            addYear(year);
+            addYear(year, false);
         }
     }
 
@@ -64,14 +61,21 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param year
      */
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n", "WrongConstant"})
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private void addYear(int year) {
+    private void addYear(int year, boolean toTop) {
         Calendar c = Calendar.getInstance();
         for (int month = 0; month < 12; month++) {
+            GridLayout gl = new GridLayout(this);
+            FrameLayout.LayoutParams params =
+                    new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+            gl.setLayoutParams(params);
+            gl.setOrientation(GridLayout.VERTICAL);
+            gl.setAlignmentMode(View.TEXT_ALIGNMENT_CENTER);
             c.set(year, month, 1);//月份从0开始
             int totalDays = c.getActualMaximum(Calendar.DAY_OF_MONTH);
-//            System.out.println(totalDays + "***" + c.get(Calendar.YEAR) + "-" + c.get(Calendar.MONTH) + "-" + c.get(Calendar.DAY_OF_MONTH));
+//            System.out.println(totalDays + "***" + c.get(Calendar.YEAR) + "-" + (c.get(Calendar.MONTH) + 1) + "-" + c.get(Calendar.DAY_OF_MONTH));
+//            System.out.println(c.get(Calendar.DAY_OF_WEEK));
             TextView tv = new TextView(this);
             tv.setBackgroundColor(getResources().getColor(R.color.colorDark));
             tv.setTextColor(getResources().getColor(R.color.colorWhite2));
@@ -79,18 +83,59 @@ public class MainActivity extends AppCompatActivity {
             tv.setTextSize(18);
             tv.setPadding(0, 10, 0, 10);
             tv.setText(months_name[month] + " " + Integer.toString(year));
-//            scrollArea.addView(tv);
+            gl.addView(tv);
             LinearLayout ll = new LinearLayout(this);
-
             ll.setOrientation(LinearLayout.HORIZONTAL);
             ll.setPadding(0, 1, 0, 0);
-            for (int day = 1; day <= totalDays; day++) {
-                MyButton btn = new MyButton(this);
-                btn.setText(Integer.toString(month) + "-" + Integer.toString(day));
-                btn.setTime(year, month, day);
-                ll.addView(btn);
+            //第一周前加入上一个月的最后几天
+            MyButton btn0 = new MyButton(this);
+            btn0.setText(Integer.toString(month + 1) + "-" + Integer.toString(1));
+            btn0.setTime(c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, c.get(Calendar.DAY_OF_MONTH));
+            c.add(Calendar.MONTH, -1);
+            c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.getActualMaximum(Calendar.DAY_OF_MONTH));
+            int week = btn0.dayOfTheWeek;//当前周已经有多少天了
+            for (int i = btn0.dayOfTheWeek - 1; i >= 1; i--) {
+                MyButton btn1 = new MyButton(this);
+                btn1.setText(Integer.toString(c.get(Calendar.MONTH) + 1) + "-" + Integer.toString(c.getActualMaximum(Calendar.DAY_OF_MONTH) - i + 1));
+                btn1.setTime(c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, c.get(Calendar.DAY_OF_MONTH) - i + 1);
+                ll.addView(btn1);
             }
-            scrollArea.addView(ll);
+            ll.addView(btn0);
+            c.add(Calendar.MONTH, 1);
+            c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH), 1);
+            for (int day = 2; day <= totalDays; day++) {
+                if (week == 7) {
+                    gl.addView(ll);
+                    ll = new LinearLayout(this);
+                    ll.setOrientation(LinearLayout.HORIZONTAL);
+                    ll.setPadding(0, 1, 0, 0);
+                    week = 0;
+                }
+                MyButton btn = new MyButton(this);
+                btn.setText(Integer.toString(month + 1) + "-" + Integer.toString(day));
+                btn.setTime(year, month + 1, day);
+                ll.addView(btn);
+                week++;
+            }
+            //最后一周后加入下一个月前几天
+            if (week == 7) {
+                gl.addView(ll);
+            } else {
+                c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH), 1);
+                c.add(Calendar.MONTH, 1);
+                c.set(c.get(Calendar.YEAR), c.get(Calendar.MONTH), 1);
+                btn0 = new MyButton(this);
+                btn0.setText(Integer.toString(c.get(Calendar.MONTH) + 1) + "-" + Integer.toString(1));
+                btn0.setTime(c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, 1);
+                for (int i = btn0.dayOfTheWeek; i <= 7; i++) {
+                    MyButton btn1 = new MyButton(this);
+                    btn1.setText(Integer.toString(c.get(Calendar.MONTH) + 1) + "-" + Integer.toString(1 + i - btn0.dayOfTheWeek));
+                    btn1.setTime(c.get(Calendar.YEAR), c.get(Calendar.MONTH) + 1, 1 + i - btn0.dayOfTheWeek);
+                    ll.addView(btn1);
+                }
+                gl.addView(ll);
+            }
+            scrollArea.addView(gl, toTop ? 0 : -1);
         }
     }
 
--- app/src/main/java/com/example/diary/MyButton.java
+++ app/src/main/java/com/example/diary/MyButton.java
@@ -1,7 +1,9 @@
 package com.example.diary;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+import androidx.annotation.RequiresApi;
 
 import java.util.Calendar;
 
@@ -12,6 +14,7 @@ public class MyButton extends androidx.appcompat.widget.AppCompatButton {
     public int dayOfTheWeek;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     public MyButton(Context context) {
         this(context, null);
         this.year = -1;
@@ -20,6 +23,8 @@ public class MyButton extends androidx.appcompat.widget.AppCompatButton {
         this.dayOfTheWeek = -1;
         this.setBackgroundColor(getResources().getColor(R.color.colorGrayDeep));
         this.setTextColor(getResources().getColor(R.color.colorWhite));
+        this.setWidth(150);
+        this.setGravity(TEXT_ALIGNMENT_CENTER);
     }
 
     public void setTime(int year, int month, int day) {
@@ -27,11 +32,11 @@ public class MyButton extends androidx.appcompat.widget.AppCompatButton {
         this.month = month;
         this.day = day;
         Calendar c = Calendar.getInstance();
-        c.set(year, month, day);
-        this.dayOfTheWeek = c.get(Calendar.DAY_OF_WEEK) + 3;
+        c.set(year, month - 1, day);
+        this.dayOfTheWeek = c.get(Calendar.DAY_OF_WEEK) - 1;
         //dayOfTheWeek:Mon Tue Wed Thu Fri Sat Sun
-        //value       :5 6 7 1 2 3 4
-        if (this.dayOfTheWeek > 7) this.dayOfTheWeek -= 7;
+        //value       :2 3 4 5 6 7 1
+        if (this.dayOfTheWeek == 0) this.dayOfTheWeek = 7;
     }
 
     public MyButton(Context context, AttributeSet attrs) {
